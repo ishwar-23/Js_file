15e04a162cdaba3783314d11bb62cef747ea853a
Ishwar <75927472+ishwar-23@users.noreply.github.com>
2023-09-24 10:03:11 +0000

    datatype done

---
+++ 01_basics/05_datatypesummery.js
@@ -0,0 +1,40 @@
+
+
+// two tyoe of data 1> primitive and non primitive.
+
+// 1> Primitive 
+
+// 7 types: number, boolean, string, null, undefined, bigInt, symbol
+
+
+const score = 10;
+const scorevalue= 100;
+
+const isloginn = false;
+const outsidetemp = null;
+let userEmail;
+
+const id = Symbol("1234")
+const idnew = Symbol("1234")
+
+console.log(id === idnew);
+
+
+
+// 2> Non- primitives (refference)
+
+// Arrar, object, function
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
