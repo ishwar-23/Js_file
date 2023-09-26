2013902429644cb1d1ef80b9ef582d45c3b98fb3
Ishwar <75927472+ishwar-23@users.noreply.github.com>
2023-09-26 09:44:25 +0000

    changes done

---
+++ 01_basics/07_numberAndmath.js
@@ -0,0 +1,47 @@
+
+
+const score = 400;
+
+// console.log(score);
+
+const balance = new Number(4000);
+
+// console.log(balance.toString());
+
+// console.log(typeof balance);
+
+// console.log(balance.toFixed(2));
+
+const othernumber = 17723.758574
+
+// console.log(othernumber.toPrecision(2));
+
+
+const hundred = 10000000;
+
+// console.log(hundred.toLocaleString());
+
+// console.log(hundred.toLocaleString("en-IN"));
+
+
+// =============++++++++++++++++++++++++++++++======================
+
+
+ console.log(Math);
+
+ console.log(Math.abs(-5));
+
+ console.log(Math.round(7.89));
+ 
+
+
+
+
+
+
+
+
+
+
+
+
