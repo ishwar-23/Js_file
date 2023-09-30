e659410457949992df74c4646f159f87502b9aae
Ishwar <75927472+ishwar-23@users.noreply.github.com>
2023-09-30 12:47:50 +0000

    math and num

--- 01_basics/07_numberAndmath.js
+++ 01_basics/07_numberAndmath.js
@@ -29,10 +29,28 @@ const hundred = 10000000;
 
  console.log(Math);
 
- console.log(Math.abs(-5));
+//  console.log(Math.abs(-5));
 
- console.log(Math.round(7.89));
- console.log(Math.round(9.89));
+//  console.log(Math.round(7.89));
+//  console.log(Math.round(9.89));
+
+
+//  console.log(Math.min(4,7,9,10));
+//  console.log(Math.max(4,7,9,10));
+
+//  console.log(Math.random());
+
+//  console.log(Math.floor((Math.random()*10) + 1));
+
+
+const min = 10;
+const max = 15;
+
+// console.log(Math.floor(Math.random()*(max - min +1)) + min);
+
+
+ 
+
  
 
 
