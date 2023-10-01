a703e6024d4c3bee5eadfb7910bc6ecdcd8c15e1
Ishwar <75927472+ishwar-23@users.noreply.github.com>
2023-10-01 08:28:04 +0000

    date and time done

--- 01_basics/08_dateandtime.js
+++ 01_basics/08_dateandtime.js
@@ -2,4 +2,66 @@
 
 var myDate = new Date();
 
-console.log(myDate);
+// console.log(myDate);
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toISOString());
+
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.getHours());
+
+
+// let mycreatedDate = new Date(2023,0,5);
+
+// console.log(mycreatedDate.toDateString());
+
+
+// let mycreatedDate = new Date(2023,0,5,7,3);
+
+let specificDate = new Date("01-09-2023")
+
+
+// console.log(mycreatedDate.toLocaleString());
+
+let mytimeStam = Date.now();
+
+// console.log(mytimeStam);
+
+// console.log( specificDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+
+newDate.toLocaleString("default",{
+
+    weekday: "long"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
