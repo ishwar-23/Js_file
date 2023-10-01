d18855506a963854c1d263bd1c7f7161fc26f87d
Ishwar <75927472+ishwar-23@users.noreply.github.com>
2023-10-01 18:39:42 +0000

    array part 1

---
+++ 02_basics/Array.js
@@ -0,0 +1,85 @@
+
+// Array can be define as  the below type.
+
+const arr2 = new Array(4,5,6,'thenew', true);
+
+// const array = [1,3,4,5,7,8];
+// console.log(array);
+// console.log(arr2);
+
+//  ********induction of the array****
+
+// console.log(array[3]);
+
+//  ********Arrya methods.******************
+
+// Push method
+// const array = [1,3,4,5,7,8];
+
+// array.push(6);
+// console.log(array);
+
+//  POP Method 
+
+// const arraynew = [8,4,5,7,2,5,7,9];
+
+// arraynew.pop();
+// console.log(arraynew);
+
+
+//  unshift and shift method .
+
+// unshift
+// const arr_2 = [4,5,63,45,78,6,0,4];
+
+// arr_2.unshift(9)
+// console.log(arr_2)
+
+// shift
+// arr_2.shift();
+// console.log(arr_2)
+
+// console.log(arr_2)
+
+
+
+//  includes  and index of 
+
+const arr_2 = [4,5,63,45,78,6,0,4];
+
+// console.log(arr_2.indexOf(63));
+
+// console.log(arr_2.includes("99"));
+// console.log(arr_2.includes(0));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
